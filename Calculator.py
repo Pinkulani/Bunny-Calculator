33825bd12a7283c458df424bae60afd949e45e7b
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-11-30 23:43:14 +0100

    Adding better main program

--- Calculator.py
+++ Calculator.py
@@ -1,17 +1,25 @@
 # @Pinkulani 30.11.2023
 
 from Modules.Arithmetic import *
+from Modules.Pythagoras import *
 
-print("Bunny Calculator")
+print("|- Bunny Calculator -| \n")
+print("Modules included:")
+
+Modules = ["Arithmetic", "Pythagoras"]
+for X in range(0, len(Modules)):
+    print("- ", Modules[X])
+
+print("")
 while True:
     Mode = int(input("Choose type: "))
     match Mode:
         case 1:
-            Add()
+            Arithmetic()
             break
         case 2:
-            print("2")
+            Pythagoras()
             break
         case _:
-            print("Invalid input")
+            print("Invalid input!")
             continue
