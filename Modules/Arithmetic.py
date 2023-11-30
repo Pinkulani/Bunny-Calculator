bb1b47cfa28fcab3348159b367206f49ddabf518
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-11-30 23:43:55 +0100

    Adding basic functions

--- Modules/Arithmetic.py
+++ Modules/Arithmetic.py
@@ -1,2 +1,14 @@
 def Add(A, B):
     return (A + B)
+
+def Sub(A, B):
+    return (A - B)
+
+def Multiply(A, B):
+    return (A * B)
+
+def Divide(A, B):
+    return (A / B)
+
+def Arithmetic():
+    Input = input("Expression: ")
