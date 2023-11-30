3e4e29c5a9723d84737f196fa81a2c6eead215ac
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-12-01 00:44:11 +0100

    Adding missing root calculation 😖

--- Modules/Pythagoras.py
+++ Modules/Pythagoras.py
@@ -1,19 +1,31 @@
+# @Pinkulani 1.12.2023
+import math
+
 def Pythagoras():
     print("Put 0 for value you want to calculate (2**2 + 0 = 5**2)")
     A = float(input("Value of A: "))
     B = float(input("Value of B: "))
     C = float(input("Value of C: "))
+    # Result sacrifices Memory instead of CPU resources
     match A:
         case 0:
             # A is unknown
-            print((C**2) - (B**2))
+            Result = (C**2) - (B**2)
+            if Result >= 0:
+                print(math.sqrt(Result))
+            else:
+                print("No solution for real numbers!")
         
         case _:
             match B:
                 case 0:
                     # B is unknown
-                    print((C**2) - (A**2))
+                    Result = (C**2) - (A**2)
+                    if Result >= 0:
+                        print(math.sqrt(Result))
+                    else:
+                        print("No solution for real numbers!")
 
                 case _:
                     # C is unknown
-                    print((A**2) + (B**2))
+                    print(math.sqrt((A**2) + (B**2)))
