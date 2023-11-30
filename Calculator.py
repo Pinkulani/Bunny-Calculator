1dfc1cd22780131c8779f1f3869c1b6cb897358a
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-11-30 21:12:57 +0000

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+# @Pinkulani 30.11.2023
+print("Bunny Calculator")
+while True:
+    Mode = int(input("Choose type: "))
+    match Mode:
+        case 1:
+            print("1")
+            break
+        case 2:
+            print("2")
+            break
+        case _:
+            print("Invalid input")
+            continue
